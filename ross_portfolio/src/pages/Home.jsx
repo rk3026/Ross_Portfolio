2c8bd2d5173c74a7e078d7cc7fca089b2ee3d5b7
Ross <rosskugler@gmail.com>
2025-05-16 00:51:02 -0700

    finish contact section and footer

--- ross_portfolio/src/pages/Home.jsx
+++ ross_portfolio/src/pages/Home.jsx
@@ -5,6 +5,8 @@ import { HeroSection } from "../components/HeroSection";
 import { AboutSection } from "../components/AboutSection";
 import { SkillsSection } from "../components/SkillsSection";
 import { ProjectsSection } from "../components/ProjectsSection";
+import { ContactSection } from "../components/ContactSection";
+import { Footer } from "../components/Footer";
 
 export const Home = () => {
     return (
@@ -23,9 +25,11 @@ export const Home = () => {
             <AboutSection />
             <SkillsSection />
             <ProjectsSection />
+            <ContactSection />
         </main>
 
         {/* Footer */}
+        <Footer />
 
         </div>
     );
