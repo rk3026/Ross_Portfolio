6fca25ad4daca47ef067e7c81a642f24ab64dc0b
Ross <rosskugler@gmail.com>
2025-05-19 18:10:13 -0700

    move app into base of repo

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,12 @@ import tailwindcss from "@tailwindcss/vite"
 import path from "path"
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react(), tailwindcss()],
+  base: mode === 'production' ? '/Ross_Portfolio/' : '/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+}))
